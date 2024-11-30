67de266419995bd4ee980febd62362f5bc8770c7
Caminh0neir0 <caua.brier06@gmail.com>
2024-11-29 23:19:43 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Língua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos estados tem o Brasil?',
+    '26 estados e um Distrito Federal, totalizando 27.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é relevo?',
+    'conjunto de saliências e reentrâncias de uma superfície, esp. da superfície da Terra.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 3+3?',
+    'A soma dos números é 6.'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica São Paulo?',
+    'São Paulo está situado no Brasil.'
+)
